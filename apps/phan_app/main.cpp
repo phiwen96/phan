a2a4f42392dc9a5cee896fb05ee7133cca2d7f5e
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-07 18:10:25 +0100

    a

--- apps/phan_app/main.cpp
+++ apps/phan_app/main.cpp
@@ -7,6 +7,15 @@
 using namespace std;
 
 
+#define PROCC2(str, max, var) var = []()->string\
+{\
+return str;\
+}();// = [&s]->string{return "";}();
+
+#define PROCC(x) PROCC2 (BOOST_PP_STRINGIZE (BOOST_PP_SEQ_ELEM (0, x)), BOOST_PP_SEQ_ELEM (1, x), BOOST_PP_SEQ_ELEM (2, x))//BOOST_PP_SEQ_ELEM (2, x) = [](string s){ \
+
+
+
 //#include <boost/preprocessor/
 #define STR(...) BOOST_
 
@@ -80,7 +89,38 @@ int main() {
 //    cout << first_signature (str, first_parser) << endl;
     
     
+    PROCC ((
+        template <int>
+        struct gpu;
+        
+        $(1)
+        {
+            € = inline
+            $ = declare variable, only visible to the current scope
+                and to those below
+            
+            template <>
+            struct gpu <$(0 i 10)>
+            {
+                int $(0 j 10) = 0;
+                
+                €[j = @(i)](0 i 3){int i@(i) = @(j);}(myFun)
+            0 i 3 prio före 0 i 10 -> därför sätter vi om namnet till j
+            0 i 3 is internal dvs endast inne i {} och i refererar inget utanför
+                $(stat){static $(con){constexpr} int} fitta = GPU_COUNT;
+            ERROR-> $(stat){kiss}
+                 @(stat) count = GPU_COUNT;
+                 ${static} constexpr uint32_t max_image_dimension_1D = GPU_${i}_MAX_IMAGE_DIMENSION_1D;
+                 @(st) int i = 3;
+            };
+        }
+           )(0)(string s));
     
+    cout << s << endl;
+        
+        
+        
+        
     
     std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
     
