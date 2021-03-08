0cb6f1bd0e4bbb8e3d377da0cee117cd610b7bf7
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-08 14:45:09 +0100

    a

--- apps/phan_app/main.cpp
+++ apps/phan_app/main.cpp
@@ -95,21 +95,79 @@ int main() {
         
         $(1)
         {
-            € = inline
-            $ = declare variable, only visible to the current scope
-                and to those below
+a       # = comment # ?(name)?{explanation}
+        
+        € = _function (_anonymous/_non-anonymous) # ?[?_scope_var = @(_var)?]?(0 ?i? 0)?{}??(_function_name)?
+        $ = _variable (_non-anonymous){_code} # (_name){}
+            @ = _paste
             
             template <>
-            struct gpu <$(0 i 10)>
+            struct gpu <${0 i 10}> # {_public change all in scope}
             {
-                int $(0 j 10) = 0;
+    
+                $(0 i 4) # {everyting refering to i will clone for times}
+                # int i${i} -> int i0123
+@(i)<1 10>
+@(i)<1 10> -> {@} 12345678910
+@<0 2> -> {ph}  phph
+  
                 
-                €[j = @(i)](0 i 3){int i@(i) = @(j);}(myFun)
-            0 i 3 prio före 0 i 10 -> därför sätter vi om namnet till j
-            0 i 3 is internal dvs endast inne i {} och i refererar inget utanför
-                $(stat){static $(con){constexpr} int} fitta = GPU_COUNT;
-            ERROR-> $(stat){kiss}
-                 @(stat) count = GPU_COUNT;
+
+                     2
+@(hej) <-> ${0#}     02  02
+                
+                    -1
+@(hej#) <-> ${0#}    -10  0-1
+                
+                     2
+@(#hej) <-> ${0#}     022  02
+
+                          2
+${0#} -> @(#hej)     002  02
+
+kuk @<1 3> &{ph&}               kuk ph1ph2ph3
+fitta @<1 3> -> {ph&} kuk       fitta ph123 kuk
+@<1 3> -> &{ph}                 phphph
+    
+
+                
+                
+
+                
+                
+@(hej) <-> {0}      0 0
+@(hej)              ()  0
+@(hej) <- {1}       1   ()
+@(hej) -> {2@}      ()  21
+@(hej)              1   ()
+@(hej) <-> {3@}     3   31
+                
+@(hej@) <-> {kuk}   "snopphejkuk"
+                
+@(hej) {philip@} philipkuk
+
+                
+  @{i} -> {int i} kommer bli int i int i int i int i
+  @{i} -> {int@ i@}  int0 i0 int1 i1 int2 i2 int3 i3
+ ${i}    0123
+                
+                $(str){philip}    "philip"
+                ${str} -> {int i@} -> {kuk @ hora}   "kuk int iphilip hora"
+$(hej){kukens fitta}   kukens fitta
+${hej}       kukens fitta
+        
+                int i${0 10} = 0; #{_private}
+                int j€(0 5)(k) = 3; #{public}
+                
+                €[j = @(i)](0 i 3){int i@(i + 1) = @(j);}(myFun)
+                
+//                €[j = @(i)](0 i 3){int i@(i) = @(j);} anonymous
+//            0 i 3 prio före 0 i 10 -> därför sätter vi om namnet till j
+//            0 i 3 is internal dvs endast inne i {} och i refererar inget utanför
+                $(_stat_int){static $(con){constexpr} int} fitta = GPU_COUNT;
+            ERROR-> $(_stat){kiss}
+                @(_stat_int) count = GPU_COUNT;
+                
                  ${static} constexpr uint32_t max_image_dimension_1D = GPU_${i}_MAX_IMAGE_DIMENSION_1D;
                  @(st) int i = 3;
             };
