1a5144f32a70bd094e140fccdf41af20f4b6da9e
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-09 16:01:50 +0100

    a

--- tests/test_phan_app/src/test_phan_app.cpp
+++ tests/test_phan_app/src/test_phan_app.cpp
@@ -1,25 +1,40 @@
 //#define CATCH_CONFIG_RUNNER
+//#include <catch2/catch.hpp>
 
 
 
 
+using namespace std;
 
 
 
-
-
-SCENARIO( "vectors can be sized and resized", "[vector]" ) {
-
-    GIVEN( "A vector with some items" ) {
-        std::vector<int> v( 5 );
-    }
-}
-
-//int main (int argc, char* argv[])
-//{
-////    assert(argc == 0);
+//SCENARIO( "vectors can be sized and resized", "[vector]" ) {
 //
-//    return 0;
+//    GIVEN( "A vector with some items" ) {
+//        std::vector<int> v( 5 );
+//    }
 //}
+
+array <string, TEST_FILE_COUNT> test_files;
+
+
+#define MACRO(z, n, text) test_files [n] = BOOST_PP_CAT (text, n);
+
+
+
+
+
+int main (int argc, char* argv[])
+{
+    BOOST_PP_REPEAT (TEST_FILE_COUNT, MACRO, TEST_FILE_)
+    
+    for (auto i : test_files)
+        cout << i << endl;
+//    assert(argc == 0);
+   
+//    cout << TARGET_FILE_0 << endl;
+
+    return 0;
+}
 
 
