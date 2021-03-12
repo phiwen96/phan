2db79b30f7218df83371f9221f505bcc3647418b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-12 10:39:45 +0100

    a

--- tests/test_phan_app/src/test_phan_app.cpp
+++ tests/test_phan_app/src/test_phan_app.cpp
@@ -6,6 +6,16 @@
 
 using namespace std;
 
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+        if (!input_file.is_open()) {
+            cerr << "Could not open the file - '"
+                 << path << "'" << endl;
+            exit(EXIT_FAILURE);
+        }
+        return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
 
 
 //SCENARIO( "vectors can be sized and resized", "[vector]" ) {
@@ -32,9 +42,24 @@ int main (int argc, char* argv[])
     
     BOOST_PP_REPEAT (TEST_FILE_COUNT, PRE, TEST_FILE_PRE_)
     BOOST_PP_REPEAT (TEST_FILE_COUNT, POST, TEST_FILE_POST_)
+    BOOST_PP_REPEAT (TEST_FILE_COUNT, FACIT, TEST_FILE_FACIT_)
     
-    for (auto i : test_files_pre)
-        cout << i << endl;
+    string warning = "";
+    
+    for (int i = 0; i < TEST_FILE_COUNT; ++i)
+    {
+        string post = readFileIntoString (test_files_post[i]);
+        string facit = readFileIntoString (test_files_facit[i]);
+ 
+        if (post != facit)
+        {
+            warning += "\n\n\t" + test_files_post[i] + "\n\t != " + "\n\t" + test_files_facit[i] + "\n\n\n";
+        }
+    }
+    if (warning != "") {
+        throw runtime_error (warning);
+    }
+       
 //    assert(argc == 0);
    
 //    cout << TARGET_FILE_0 << endl;
