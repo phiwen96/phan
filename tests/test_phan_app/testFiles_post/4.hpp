169286324f6f7f67a9cf2c2daeb28842b3589ebc
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-15 13:32:51 +0100

    a

--- tests/test_phan_app/testFiles_post/4.hpp
+++ tests/test_phan_app/testFiles_post/4.hpp
@@ -1,8 +1,39 @@
 bajs
 hor
 baj
+K
+
+    
+        int j0;
+    
+        int j1;
+    
+    int a3k 3
+
+    
+        int j0;
+    
+        int j1;
+    
+    int a4k 4
+
+    
+        int j0;
+    
+        int j1;
+    
+    int a5k 5
+
+    
+        int j0;
+    
+        int j1;
+    
+    int a6k 6
+
 int a3k 3int a4k 4int a5k 5int a6k 6
 KK
+int a3k 3int a4k 4int a5k 5int a6k 6
 
 
 
--- tests/test_phan_app/testFiles_pre/4.hpp
+++ tests/test_phan_app/testFiles_pre/4.hpp
@@ -5,8 +5,16 @@ bajs
 @(bajs){BAJS}
 hor
 baj
+$(k){K}
+$(3 x 7){
+    $(0 y 2){
+        int j${y};
+    }
+    int a${x}k ${x}
+}
 $(3 x 7){int a${x}k ${x}}
 $(kk){KK}
+$(3 x 7){int a${x}k ${x}}
 
 
 
